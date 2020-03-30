6087061c9b0831efee402136573a9674bbe24d6d
nawZeroX239 <naw122333444455555@gmail.com>
2020-03-30 15:29:47 -0700

    WTF

---
+++ UVAOJ/UVAOJ1124.java
@@ -0,0 +1,16 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UVAOJ1124 {
+	// AC WTF?
+	public static void solve() throws IOException{
+		BufferedReader br = new BufferedReader(
+									new InputStreamReader(System.in));
+		String L = null;
+		while((L=br.readLine())!=null) {
+			System.out.println(L);
+		}
+	}
+}
