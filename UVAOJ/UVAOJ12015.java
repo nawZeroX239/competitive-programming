9114c117dc72e13de184df0ced80a21968eabc0d
nawZeroX239 <naw122333444455555@gmail.com>
2020-04-04 23:55:11 -0700

    Google is Feeling Lucky

--- UVAOJ/UVAOJ12015.java
+++ UVAOJ/UVAOJ12015.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -22,26 +21,29 @@ public class UVAOJ12015 {
 		}
 
 		int T=sc.nextInt();
+		Queue<String> q = new LinkedList<>();
 		for(int I=1; I<=T; I++) {
-			HashMap<Integer, Queue<String>> map=new HashMap<>();
 
-			int largest=1;
+			String s=sc.next();
+			int largest=sc.nextInt();
+			q.add(s);
 			System.out.println("Case #"+I+":");
-			for(int i=0; i<10; i++) {
-				String s=sc.next();
-				int V=sc.nextInt();
-				if(V>largest) largest=V;
-				if(map.get(V)==null) {
-					map.put(V, new LinkedList<String>());
-				}
-				map.get(V).add(s);
+			for(int i=1; i<10; i++) {
+				s=sc.next();
+				int temp=sc.nextInt();
+				if(temp>largest) {
+					q.clear();
+					largest=temp;
+					q.add(s);
+				} else if(temp==largest) q.add(s);
 			}
-			Queue<String> q=map.get(largest);
-			while(q.size()>0) {
+			
+			while(q.size()>0)
 				System.out.println(q.remove());
-			}
+			
 		}
 		sc.close();
 	}
+
 
 }
