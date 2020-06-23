7ad36e7d0f84f3711b9b63b6396da72ab7254939
nawZeroX239 <naw122333444455555@gmail.com>
2020-06-23 12:57:09 -0700

    Extremely Basic

---
+++ URI/1001.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <forward_list>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#define EPS 1e-100
+#define N 30001
+#define ll long long
+#define pi pair<ll, ll>
+
+using namespace std;
+
+inline void newline() { cout << '\n'; }
+inline void split(vector<string>& vect, string& str, char delim) {
+    string each;
+    for (auto it = str.begin(); it != str.end(); ++it) {
+        if (*it != delim)
+            each.push_back(*it);
+        else {
+            vect.push_back(each);
+            each.clear();
+        }
+    }
+    if (each.size()) vect.push_back(each);
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+    int a, b;
+    cin >> a >> b;
+    cout <<"X = "<<a + b << '\n';
+}
