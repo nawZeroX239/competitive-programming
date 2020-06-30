da00baa402e8d4e2fa233121af6c5c17cb69211a
nawZeroX239 <naw122333444455555@gmail.com>
2020-06-30 02:13:24 -0700

    Better Two Sets

--- CSES/Two Sets.cpp
+++ CSES/Two Sets.cpp
@@ -27,7 +27,7 @@
 #include <vector>
 
 #define EPS 1e-9
-#define N 1000001
+#define N 500005
 #define PIXEL 1024
 #define ll long long
 #define pi pair<ll, ll>
@@ -56,55 +56,40 @@ inline bool lb_pred(const pair<int, int>& left, int right) {
 	return left.first < right;
 }
 
+int P[N], Q[N];
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	int  n, sz;
-	cin >> n;
-	sz = n/2;
-	if (sz == 0 ||(sz%2 && n%2==0) || (sz%2==0 && n%2)) cout << "NO\n";
+	int  n, setSize, lo, hi, pSize, qSize;
+	scanf("%d", &n);
+	setSize = n/2;
+	if (setSize == 0 ||(setSize%2 && n%2==0) || (setSize%2==0 && n%2)) cout << "NO\n";
 	else {
-		cout << "YES\n";
-		vector<int> P, Q;
-		int lo = sz;
-		int hi = sz + 1;
-		while (lo > 0) {
-			if (P.size() < sz) {
-				P.push_back(lo);
-				P.push_back(hi);
-			} else {
-				Q.push_back(lo);
-				Q.push_back(hi);
+		pSize = qSize = 0;
+		for (lo = setSize, hi = setSize + 1; lo > 0; --lo, ++hi) {
+			if (pSize < setSize) {
+				P[pSize++] = lo;
+				P[pSize++] = hi;
 			}
-			--lo;
-			++hi;
+			else {
+				Q[qSize++] = lo;
+				Q[qSize++] = hi;
+			}
 		}
 
 		if (n&1) {
-			if (P.size() < Q.size()) P.push_back(hi);
-			else Q.push_back(hi);
+			if (pSize < qSize) P[pSize++] = hi;
+			else Q[qSize++] = hi;
 		}
-
-		cout << P.size() << '\n';
-		auto it1 = P.begin();
-		auto it2 = Q.begin();
-		if (it1 != P.end()) {
-			cout << *it1;
-			++it1;
-			for (; it1 != P.end(); ++it1) {
-				cout << ' ' << *it1;
-			}
-			cout << '\n';
-		}
-		cout << Q.size() << '\n';
-		if (it2 != Q.end()) {
-			cout << *it2;
-			++it2;
-			for (; it2 != Q.end(); ++it2) {
-				cout << ' ' << *it2;
-			}
-			cout << '\n';
-		}
+		puts("YES");
+		printf("%d\n%d", pSize, P[0]);
+		for (int i = 1; i < pSize; ++i)
+			printf(" %d", P[i]);
+		puts("");
+		printf("%d\n%d", qSize, Q[0]);
+		for (int i = 1; i < qSize; ++i)
+			printf(" %d", Q[i]);
+		puts("");
 	}
 }
