954d6b75a123efe305b6185dc087d87d6c486a15
nawZeroX239 <naw122333444455555@gmail.com>
2020-07-10 13:47:14 -0700

    Ecological Bin Packing - more compact

--- UVAOJ/102.cpp
+++ UVAOJ/102.cpp
@@ -30,50 +30,32 @@
 int arr[N];
 using namespace std;
 
-int get(char c) {
+inline int get(const char c) {
 	if (c == 'B') return 0;
-	if (c == 'C') return 1;
-	return 2;
-}
-
-int match(int x) {
-	if (x == 1) return 2;
-	if (x == 2) return 1;
-	return 0;
+	return (c == 'C' ? 2 : 1);
 }
 
 int main() {
-	int b, g, c;
-	string m = "BCG";
-	vector<string> S;
-	do {
-		S.push_back(m);
-	} while (next_permutation(m.begin(), m.end()));
-	while (cin >> arr[0]) {
+	int b, g, c, ans, idx, x;
+	const char* P[] = { "BCG", "BGC", "CBG", "CGB", "GBC", "GCB" };
+
+	while (scanf("%d", &arr[0]) == 1) {
 		for (int i = 1; i < N; ++i)
-			cin >> arr[i];
-		b = g = c = 0;
-		for (int r = 0; r < 3; ++r) {
-			b += arr[3 * r + 0];
-			g += arr[3 * r + 1];
-			c += arr[3 * r + 2];
-		}
+			scanf("%d", &arr[i]);
 
-		int ans = INT_MAX;
-		int idx = 0;
+		b = arr[0] + arr[3] + arr[6];
+		g = arr[1] + arr[4] + arr[7];
+		c = arr[2] + arr[5] + arr[8];
 
-		for (int i = 0; i < S.size(); ++i) {
-			int x = 0;
-			x += b - arr[3 * 0 + match(get(S[i][0]))];
-			x += c - arr[3 * 1 + match(get(S[i][1]))];
-			x += g - arr[3 * 2 + match(get(S[i][2]))];
-
+		ans = INT_MAX;
+		idx = 0;
+		for (int i = 0; i < 6; ++i) {
+			x = b + c + g- arr[get(P[i][0])] - arr[3 + get(P[i][1])] - arr[6 + get(P[i][2])];
 			if (x < ans) {
 				ans = x;
 				idx = i;
 			}
 		}
-
-		cout <<S[idx]<<' '<<ans<<'\n';
+		printf("%s %d\n", P[idx], ans);
 	}
 }
