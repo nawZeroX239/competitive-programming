19a94a0c788cc45f8f993e4848aa4f75fb4bb56f
nawZeroX239 <naw122333444455555@gmail.com>
2020-07-10 14:07:11 -0700

    Ecological Bin Packing - more compact

--- UVAOJ/102.cpp
+++ UVAOJ/102.cpp
@@ -1,56 +1,37 @@
 #include <iostream>
-#include <algorithm>
-#include <climits>
-#include <utility> 
-#include <unordered_set>
-#include <vector>
-#include <list> 
-#include <string>
-#include <iterator> 
-#include <deque>
-#include <array>
-#include <forward_list>
-#include <queue>
-#include <stack>
-#include <sstream>
-#include <string>
-#include <set>
-#include <map>
-#include <numeric>
-#include <unordered_set>
-#include <unordered_map>
 #include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <cmath>
+#include <climits>
+#include <unordered_map>
+
 
 #define N 9
 
-int arr[N];
 using namespace std;
 
-inline int get(const char c) {
-	if (c == 'B') return 0;
-	return (c == 'C' ? 2 : 1);
-}
+int arr[N];
+const char* P[] = { "BCG", "BGC", "CBG", "CGB", "GBC", "GCB" };
 
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
 	int b, g, c, ans, idx, x;
-	const char* P[] = { "BCG", "BGC", "CBG", "CGB", "GBC", "GCB" };
+	int total;
+	unordered_map<char, int> table;
+	table['B'] = 0;
+	table['G'] = 1;
+	table['C'] = 2;
 
 	while (scanf("%d", &arr[0]) == 1) {
-		for (int i = 1; i < N; ++i)
+		total = arr[0];
+		for (int i = 1; i < N; ++i) {
 			scanf("%d", &arr[i]);
-
-		b = arr[0] + arr[3] + arr[6];
-		g = arr[1] + arr[4] + arr[7];
-		c = arr[2] + arr[5] + arr[8];
-
+			total += arr[i];
+		}
 		ans = INT_MAX;
 		idx = 0;
 		for (int i = 0; i < 6; ++i) {
-			x = b + c + g- arr[get(P[i][0])] - arr[3 + get(P[i][1])] - arr[6 + get(P[i][2])];
+			x = total - arr[ table[P[i][0]] ] - arr[3 + table[P[i][1]] ] - arr[6 + table[P[i][2]] ];
 			if (x < ans) {
 				ans = x;
 				idx = i;
