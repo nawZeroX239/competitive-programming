d3812659f8db7b33ac054ee76455cc13dd506689
nawZeroX239 <naw122333444455555@gmail.com>
2020-07-19 20:38:11 -0700

    Ecosystem

--- UVAOJ/626.cpp
+++ UVAOJ/626.cpp
@@ -1,65 +1,60 @@
+#include <iostream>
+#include <string>
 #include <algorithm>
+#include <utility> 
+#include <unordered_set>
+#include <vector>
+#include <list> 
+#include <string>
+#include <iterator> 
+#include <deque>
 #include <array>
-#include <bitset>
-#include <climits>
-#include <cmath>
+#include <forward_list>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
-#include <deque>
-#include <forward_list>
-#include <iomanip>
-#include <iostream>
-#include <iterator>
-#include <list>
-#include <map>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
-
-#define EPS 1e-9
-#define N 1000005
-
+#include <cmath>
+#include <climits>
+#include <bitset>
 using namespace std;
 
+#define N 100
+#define EPS 1e-9
+
 int arr[N][N];
-int main(){
+
+int main() {
   int n;
-  while(cin>>n){
-    for(int i=0;i<n;++i)
-    for(int j=0; j<n; ++j)
-      cin>>arr[i][j];
-    int total=0;
-    map<int, bool> vis;
-    for(int i=0; i<n; ++i){
-      for(int j=0; j<n; ++j) {
-        if(i == j) continue;
-        if(arr[i][j]){
-          for(int k=0; k<n; ++k) {
-            if(k == i || k == j) continue;
-            int a=100*(i+1) + 10*(j+1) + k+1;
-            int b=100*(j+1) + 10*(k+1) + i+1;
-            int c=100*(k+1) + 10*(i+1) + j+1;
-            if(arr[j][k] && arr[k][i] && !vis[a]) {
-              cout<<i+1<<' '<<j+1<<' '<<k+1<<'\n';
-              vis[a] =  vis[b] = vis[c]= true;
-              ++total;
-            }
+  while (cin >> n) {
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < n; ++j)
+        cin >> arr[i][j];
+    int total = 0;
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (i == j) continue;
+        if (arr[i][j]) {
+          // i eats j
+          for (int k = 0; k < n; ++k) {
+            if (k == i || k == j) continue;
+            if (arr[j][k] && arr[k][i])
+              // j eats k & if k eats i then its a cyclic
+              if ((i > j && j > k) || (i < j && j < k)) {
+                cout << i + 1 << ' ' << j + 1 << ' ' << k + 1 << '\n';
+                ++total;
+              }
           }
         }
       }
     }
 
-    cout<<"total:"<<total<<'\n';
-    cout<<'\n';
+    cout << "total:" << total << "\n\n";
   }
-
 }
